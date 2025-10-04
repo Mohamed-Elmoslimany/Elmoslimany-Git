59d2f22662a4e17d5124979ea2fbb5e25bfafa32
Mohamed Elmoslimany <elmoslimanym@gmail.com>
2025-10-04 18:43:50 +0300

    Grok_ed 1.0

--- GUI_pyqt/Trace_GUI.py
+++ GUI_pyqt/Trace_GUI.py
@@ -10,116 +10,54 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 
-
 flag = True
 
 class MainWindow(QMainWindow):
-
-    def __init__(self, *args, GUI_type = None, **kwargs):
+    def __init__(self, *args, tracked_dict=None, **kwargs):
         super().__init__()
         self.setWindowTitle('Trace GUI')
+        self.setCentralWidget(QWidget())
+        self.hbox1 = QHBoxLayout(self.centralWidget())
+        self.vbox = QVBoxLayout(self.centralWidget())
+
+        self.tracked_dict = tracked_dict or kwargs  # Use provided dict or kwargs
+        self.labels = {}  # Store QLabel objects for each tracked variable
+
+        self.setStyleSheet(
+            "QLabel{border: 2px solid black; border-radius: 5px; margin: 5px;}"
+            "QHBoxLayout{border: 2px solid black; border-radius: 5px; margin: 5px;}"
+            "QVBoxLayout{border: 2px solid black; border-radius: 5px; margin: 5px;}"
+        )
+
+        self.initUI()
+        self.trace()
+        Thread(target=self.update, daemon=True).start()
         self.show()
-
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        self.hbox1 = QHBoxLayout(central_widget)
-        self.hbox2 = QHBoxLayout(central_widget)
-        self.vbox = QVBoxLayout(central_widget)
-
-        print(args, kwargs)
-
-        self.GUI_type = GUI_type
-        self.args = kwargs.keys()
-        print(f"\n{kwargs = }\n{self.args = }\n")
-        self.general_trace = []
-        # self.flag = True
-
-        # for arg in args:
-            # label = QLabel(f"{arg= }", self)
-            # obj = Tracer(arg, arg, label)
-            # self.general_trace.append(label)
-            # self.general_trace.append(QLabel(f"{obj.name= }, {obj.value= }", self))
-
-        # for k, v in kwargs.items():
-            # obj = Prepare(k, v)
-            # self.general_trace.append(QLabel(f"{obj.name= }, {obj.value= }", self))
-
-        self.setStyleSheet("QLabel{border: 2px solid black; border-radius: 5px; margin: 5px;}"
-                           "QHBoxLayout{border: 2px solid black; border-radius: 5px; margin: 5px;}"
-                           "QVBoxLayout{border: 2px solid black; border-radius: 5px; margin: 5px;}")
-        self.trace(*args, **kwargs)
-        # self.initUI()
-        Thread(target=self.update, args=kwargs, daemon=True).start()
 
     def initUI(self):
         self.vbox.addLayout(self.hbox1)
-        self.vbox.addLayout(self.hbox2)
-
-        for label in self.general_trace:
+        for key, value in self.tracked_dict.items():
+            label = QLabel(f"{key}: {value}", self)
+            self.labels[key] = label
             self.hbox1.addWidget(label)
 
-    def on_button_click(self):
-        pass
-
-    def update(self, *args):
-        # print(*self.glbl.items(), sep = ", ")
-        # for v in self.args:
-            # print(f"{v}: ")
-        print()
-        print(f"{args = }\n{list(self.glbl.keys()) = }\n")
-        # for v, k in list(self.glbl.items()):
-            # print(f"{v}: {k}")
+    def update(self):
         while flag:
-            # print(self.args)
-            # print(label2)
-            # for i, arg in enumerate(self.args):
-                # self.general_trace[i].setText(f"{arg= }")
+            for key, label in self.labels.items():
+                value = self.tracked_dict.get(key, "N/A")
+                label.setText(f"{key}: {value}")
             time.sleep(1)
-            for arg in args:
-                print(self.glbl[arg])
 
-    def trace(self, *args, **kwargs):
-        self.glbl = globals()
-        self.args = args
-        for arg in args:
-            self.glbl[arg] = arg
-        for k, v in kwargs.items():
-            self.glbl[k] = v
+    def trace(self):
+        pass  # No need for glbl; tracked_dict is used directly
 
     def closeEvent(self, event):
         global flag
         flag = False
         event.accept()
 
-
-class Tracer():
-    def __init__(self, name, value, parent = "general"):
-        self.name = name
-        self.value = value
-        self.parent = parent
-
-    def __setattr__(self, name: str, value: Any) -> None:
-        pass
-
-
-def main(*args, **kwargs):
+def main(*args, tracked_dict=None, **kwargs):
     global window1
     app = QApplication(sys.argv)
-    window1 = MainWindow(*args, **kwargs)
+    window1 = MainWindow(*args, tracked_dict=tracked_dict, **kwargs)
     sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    label1 = time.time()
-    label2 = 123
-    label3 = "My name is Ahmed"
-    my_dict = {
-        "label1": label1, 
-        "label2": label2, 
-        "label3": label3
-        }
-    Thread(target=main, args=(), kwargs=my_dict, daemon=True).start()
-    while flag:
-        # print(my_dict)
-        label2 += 1
-        # print(label2)
-        time.sleep(1)
